b42e57fe2810889a4c338402cb717d63978286a7
caiyanfeng <904727221@qq.com>
2022-02-20 11:42:45 +0800

    2022.2.19 redis批量

--- src/main/java/com/cyf/springboot/demo/demo/util/redis/JedisBatch.java
+++ src/main/java/com/cyf/springboot/demo/demo/util/redis/JedisBatch.java
@@ -22,7 +22,13 @@ public class JedisBatch {
      * @return
      */
     Map<String,String> batchGetByPipeline(List<String> keys){
-        jedisCluster.mget("a");
+        /**
+         * cluster中如果计算出来不在同一个槽位不允许使用mget，mset等批量操作命令
+         * 需要批量操作需要调用jediscluster中代码计算每一个key对应的槽位，根据槽位计算node，再通过
+         * 再对涉及到的每个node使用pipeline发送批量处理命令
+         * github中找代码
+         * mo-service中已实现
+         * jedisCluster.mget("a");*/
         return null;
     }
 }
